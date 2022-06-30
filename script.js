a29b2bd5e7684d47a041c8a3422962a8bc8e9c54
Cade <cjayenn@gmail.com>
2022-06-30 12:22:10 -0400

    expanded player functions, working on turn management

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gameBoard = (() => {
     let board = [
-        '','','',
-        '','','',
-        '','',''
+        '','','',  // 0, 1, 2
+        '','','',  // 3, 4, 5
+        '','',''   // 6, 7, 8
     ]
 
     //creates divs for gameboard
-    const createSpaces = () => {
+    const _createSpaces = () => {
         const gameContainer = document.getElementById('game');
         for (let i = 0; i < 9; i++){
             const div = document.createElement('div');
@@ -16,37 +16,73 @@ const gameBoard = (() => {
         }
     }
 
-    // writes x's or o's on gameboard
-    const writeBoard = () => {
+    const _setSpaceEventListeners = () => {
+        for (let i = 0; i < 9; i++) {
+            const space = document.getElementById(`space-${i}`);
+            space.addEventListener('click', () => {
+                if (board[i] == ''){
+                    writeBoard(i, playersign);
+                }
+            });
+        }
+    }
+
+    // loads board array and displays x's and o's
+    const loadBoard = () => {
         for (let i = 0; i < 9; i++){
             document.getElementById(`space-${i}`).textContent = board[i];
         }
     }
 
-    const setEventListeners = () => {
-        for (let i = 0; i < 9; i++) {
-            const space = document.getElementById(`space-${i}`);
-            space.addEventListener('click', () => {
-                if (board[i] == ''){
-                    board[i] = 'x';
-                }
-                
-                writeBoard();
-            });
-
-        }
+    // writes sign (x or o) at space on board and reloads it
+    const writeBoard = (space, sign) => {
+        board[space] = sign;
+        loadBoard();
     }
 
+
+    // INITIALIZE BOARD
+    _createSpaces();
+    _setSpaceEventListeners();
+
     return {
-        createSpaces,
-        writeBoard,
-        setEventListeners
+        loadBoard,
+        writeBoard
     };
 })();
 
-const Player = (name) => {
-    return { name };
+const gameController = (() => {
+
+
+    const _changePlayerTurns = () => {
+        playerOne.switchTurn();
+        playerTwo.switchTurn();
+    };
+
+
+    const _winConditions = [[0,1,2],[3,4,5],[6,7,8], // row wins
+                            [0,3,6],[1,4,7],[2,5,8], // column wins
+                            [0,4,7],[2,4,6]];        // diagonal wins
+
+    //const checkForWin = () => {
+
+    //};
+})();
+
+
+const Player = (name, sign, goesFirst) => {
+    let isMyTurn = goesFirst;
+
+    const switchTurn = () => isMyTurn = !isMyTurn;
+
+    const getTurn = () => isMyTurn;
+
+    return { name, sign, getTurn, switchTurn };
 };
 
-gameBoard.createSpaces();
-gameBoard.setEventListeners();
+const playerOne = Player('Cade', 'x', true);
+const playerTwo = Player('Mark', 'o', false);
+
+console.log(playerOne.getTurn());
+playerOne.switchTurn();
+console.log(playerOne.getTurn());
